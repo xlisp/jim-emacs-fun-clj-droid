35df5eb7ffe7dc725e8f5f516e07903a6966da9c
stevechan <chanshunli@gmail.com>
2019-06-30 14:15:35 +0800

    add droid import Environment and test

--- jim0.clj
+++ jim0.clj
@@ -1,2 +1,18 @@
-(+ 1 2)
-(map #(+ % 1) [1 2 3])
+(ns test
+  (:import [android.os Build
+            Environment
+            UserManager]
+           [android.system Os]
+           [android.util Log Pair]
+           [android.view WindowManager]
+           [com.termux R]))
+
+  ;;File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+  ;;Os.symlink(downloadsDir.getAbsolutePath(), new File(storageDir, "downloads").getAbsolutePath());
+
+(Log/d "123" "000")
+
+(->
+ Environment/DIRECTORY_DOWNLOADS
+ Environment/getExternalStoragePublicDirectory)
+
